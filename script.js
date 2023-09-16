36dd96f61607937844b89bf05529f15008cbe937
MuilDjamil <luckyguy982@gmail.com>
2023-09-16 17:12:02 +0800

    add profile link

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const initApp = () => {
+  const profileOverlay = document.getElementById("profile-overlay")
+  const profileLink = document.getElementsByClassName("profile-link")
+  const profileClose = document.getElementById("profile-close")
+  const navbarTie = document.getElementById("navbar-tie")
   const mobileToggler = document.getElementById("mobile-toggler")
   const mobileNavigation = document.getElementById("mobile-navigation")
 
@@ -9,6 +13,30 @@ const initApp = () => {
   mobileNavigation.addEventListener("click", () => {
     mobileNavigation.classList.remove("show")
   })
+
+  profileLink[0].addEventListener("click", () => {
+    profileOverlay.classList.toggle("show")
+  })
+
+  profileLink[1].addEventListener("click", () => {
+    profileOverlay.classList.toggle("show")
+  })
+
+  profileClose.addEventListener("click", () => {
+    profileOverlay.classList.remove("show")
+  })
+
+  window.addEventListener("scroll", () => {
+    const verticalScrollPx = window.scrollY
+  
+    if (verticalScrollPx > 100) {
+      navbarTie.classList.add('scollup');
+    } else {
+      navbarTie.classList.remove('scollup')
+    }
+  })
 }
 
 document.addEventListener("DOMContentLoaded", initApp)
+
+
